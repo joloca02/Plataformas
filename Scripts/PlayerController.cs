4c338fc270cdae5dd351c8d22a58d660e487bd13
DESKTOP-LALVV45\chema <joloca02@floridauniversitaria.es>
2022-02-07 17:23:01 +0100

    Ejercicio 4

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    Rigidbody2D PlayerRB;
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayerRB = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float movimiento = Input.GetAxis("Horizontal");
+        PlayerRB.velocity = new Vector2(movimiento * 3, PlayerRB.velocity.y);
     }
 }
